c08aff198181e4a767acff849fb717b421e2a644
Elliott Marquez <emarquez@google.com>
2018-06-22 17:04:40 -0700

    auto-converted by polymer-modulizer

--- test/test-dialog.js
+++ test/test-dialog.js
@@ -1,4 +1,4 @@
-<!--
+/**
 @license
 Copyright (c) 2015 The Polymer Project Authors. All rights reserved.
 This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
@@ -6,21 +6,20 @@ The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
 The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
 Code distributed by Google as part of the polymer project is also
 subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
--->
+*/
+import '@polymer/polymer/polymer-legacy.js';
 
-<link rel="import" href="../../polymer/polymer.html">
+import { PaperDialogBehavior } from '../paper-dialog-behavior.js';
+import '../paper-dialog-shared-styles.js';
+import { Polymer } from '@polymer/polymer/lib/legacy/polymer-fn.js';
+import { html } from '@polymer/polymer/lib/utils/html-tag.js';
 
-<dom-module id="test-buttons">
-  <template>
-    <button dialog-dismiss id="dismiss">dismiss</button>
-    <button dialog-confirm id="confirm">confirm</button>
-  </template>
+Polymer({
+  _template: html`
+    <style include="paper-dialog-shared-styles"></style>
+    <slot></slot>
+`,
 
-  <script>
-    (function() {
-    'use strict';
-
-    Polymer({is: 'test-buttons'});
-    })();
-  </script>
-</dom-module>
+  is: 'test-dialog',
+  behaviors: [PaperDialogBehavior]
+});
